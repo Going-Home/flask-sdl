85c767d393929db2fbdf985eee2dff4324236977
Einstrasse <hg9587@naver.com>
2017-08-22 19:09:53 -0700

    dummy page

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
 	return "Hello World"
+
+@app.route('/login')
+def login_page():
+	return render_template("login.html")
 
 if __name__=="__main__":
 	app.run()
