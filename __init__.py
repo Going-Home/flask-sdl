33dcae320ca90d1e111dc3b8a15f292cd075ab52
root <root@localhost.localdomain>
2017-08-22 22:31:16 -0400

    add login-page hard coding

--- __init__.py
+++ __init__.py
@@ -1,16 +1,22 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return "Hello World"
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('secret'))
+    return render_template('login.html', error=error)
 
-@app.route('/login')
-def login_page():
-	return render_template("login.html")
+@app.route('/secret')
+def secret():
+    return "This is a secret page!"
 
 if __name__=="__main__":
 	app.run()
+
 
